1e63c53330d8f216ca0cf8e094657156e24bc132
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-18 19:39:18 +0500

    Completed the Navbar for whole app

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { assets } from "../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const [user, setUser] = useState(true); //useStste is null because the user is not login yet
+  
   const navigate = useNavigate(); //Navigate from one page to other this is react-router-dom hook
 
   return (
@@ -13,17 +13,17 @@ const Navbar = () => {
       </Link>
       <div>
         {user ? ( //Agar userlogin ha tu ye  wala div display hoga
-          <div>
-            <button>
+          <div className="flex text-center gap-2 sm:gap-3">
+            <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700"  >
                 <img className="w-5" src={assets.credit_star} alt="" />
-                <p>Credit Left : 50</p>
+                <p className="text-xs sm:text-sm font-medium text-gray-600" >Credit Left : 50</p>
             </button>
-            <p>Hi, Wajahat</p>
+            <p className="text-gray-600 max-sm:hidden pl-4" >Hi, Wajahat</p>
             <div className="relative group" >
                 <img className="w-10 drop-shadow" src={assets.profile_icon} alt="" />
                 <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12 " >
-                    <ul>
-                        <li>Logout</li>
+                    <ul className="list-none m-0 p-2 bg-white rounded-md border text-sm" >
+                        <li className="py-1 px-2 cursor-pointer pr-10">Logout</li>
                     </ul>
                 </div>
             </div>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
-    <App />
+    <AppContextProvider> {/*This provides   */}
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 )
