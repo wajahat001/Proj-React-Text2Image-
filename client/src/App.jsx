bb55a602aa61dfcff24c85ec2a7bc442b3872568
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-18 18:10:52 +0500

    All Routes are setup completely

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50' >
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredit />} />
+      </Routes> 
+    </div>
   )
 }
 
