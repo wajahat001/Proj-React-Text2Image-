8cece9d850e90e48c9988e3b8114c35b3f55ec26
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-24 16:21:27 +0500

    Resolving errors

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,8 +15,6 @@ const Navbar = () => {
       </Link>
       <div>
         {user ? ( //Agar userlogin ha tu ye  wala div display hoga
-        
-        
           <div className="flex items-center gap-2 sm:gap-3">
             <button onClick={()=>navigate('/buy')} className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700"  >
                 <img className="w-5" src={assets.credit_star} alt="" />
