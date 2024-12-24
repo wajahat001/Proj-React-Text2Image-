7a62cf630953063371c3b728789857cb1a56aad7
Obaid069 <221069@students.au.edu.pk>
2024-12-24 09:21:29 +0500

    integrated a useModel

---
+++ server/models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required:true},
+    email: {type: String, required:true, unique:true},
+    password: {type: String, required:true},
+    creditBalance: {type: Number, defaut:5}
+})
+
+const useModel = mongoose.models.user || mongoose.model("user",userSchema)
+
+export default useModel;
