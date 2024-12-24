c0effc7304a56a290ce32fccbe304a67acd81e1f
Obaid069 <221069@students.au.edu.pk>
2024-12-24 09:14:01 +0500

    integrated mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-// import connectDB from './config/mongodb.js'
+import connectDB from './config/mongodb.js'
 
 const PORT = process.env.PORT || 4000
 const app = express()
 app.use(express.json())
 app.use(cors())
-// await connectDB()
+await connectDB()
 
 
 app.get('/' , ( req , res )=> res.send("API Working fine."))
