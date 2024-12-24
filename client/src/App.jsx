8a31e6a1d11499a3d8208d17676fb5ac0aba6173
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-24 18:41:22 +0500

    Simple login form created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import Result from './pages/Result';
 import BuyCredit from './pages/BuyCredit';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './components/Login';
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50' >
     <Navbar />
+    <Login />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/result' element={<Result />} />
