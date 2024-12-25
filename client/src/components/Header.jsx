5a29baedef2600a86500a886bb2ad29777f3f0cd
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-25 11:06:37 +0500

    Added animation to web page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import {assets} from '../assets/assets'
+import {delay, motion} from 'framer-motion'
 
 const Header = () => {
+  const onClickHandler= () =>{
+    
+  }
+
+
   return (
-    <div className='flex flex-col justify-center items-center text-center my-20' >
-        <div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500' >
+    <motion.div className='flex flex-col justify-center items-center text-center my-20'
+    initial={{opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, y:0}}
+    viewport={{once:true}}
+    >
+        <motion.div className='text-stone-500 inline-flex text-center gap-2 bg-white px-6 py-1 rounded-full border border-neutral-500' 
+         initial={{opacity:0, y:-20}}
+         animate={{opacity:1, y:0}}
+         transition={{delay:0.2,duration:0.8}}
+        >
             <p>Best Text to Image generator</p>
             <img src={assets.star_icon} alt="" />
-        </div>
-        <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center ' >Turn text to <span className='text-blue-600' >image,</span>  in seconds</h1>
+        </motion.div>
+
+        <motion.h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center ' >Turn text to <span
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{delay:0.4, duration:2}}
+        className='text-blue-600' >image,</span>  in seconds</motion.h1>
         
         <p className='text-center max-w-xl mx-auto mt-5' >Unleash your creativity withh Ai Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam harum neque esse eaque distinctio provident nesciunt iure asperiores vitae ipsa.</p>
 
-        <button className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full' >Generate Images <img className='h-6' src={assets.star_group} alt="" /> </button>
+        <button onClick={onClickHandler} className='sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full' >Generate Images <img className='h-6' src={assets.star_group} alt="" /> </button>
 
         <div className='flex flex-wrap justify-center mt-16 gap-3' >
             {Array(6).fill('').map((item,index)=>(
@@ -20,7 +40,7 @@ const Header = () => {
             ))}
         </div>
         <p className='mt-2 text-neutral-600' >Generated Images from Imagify</p>
-    </div>
+    </motion.div>
   )
 }
 
