456689209459455de1517eded980c4322f42ac85
wajahat001 <wajahatchaudy1640@gmail.com>
2024-12-25 17:42:21 +0500

    Resolving error

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,94 @@
+import React, { useContext, useEffect, useState } from "react";
+import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
+
+const Login = () => {
+  const [state, setState] = useState("Login");
+  const { setShowLogin } = useContext(AppContext);
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
+  return (
+    <div className="fixed left-0 top-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <form className="relative bg-white p-10 rounded-xl text-slate-500 ">
+        <h1 className="text-center text-neutral-700 text-2xl font-medium">
+          {state}
+        </h1>
+        <p className="text-sm">Welcome back! Please SignIn to continue</p>
+
+        {state !== "Login" && (
+          <div className="border px-6 py-2  flex items-center mt-5 gap-2 rounded-full">
+            <img src={assets.user_icon} alt="" />
+            <input
+              className="outline-none text-sm"
+              type="text"
+              placeholder="Full Name"
+              required
+            />
+          </div>
+        )}
+
+        <div className="border px-6 py-2  flex items-center mt-4 gap-2 rounded-full">
+          <img src={assets.email_icon} alt="" />
+          <input
+            className="outline-none text-sm"
+            type="email"
+            placeholder="Email id"
+            required
+          />
+        </div>
+        <div className="border px-6 py-2  flex items-center mt-4 gap-2 rounded-full">
+          <img src={assets.lock_icon} alt="" />
+          <input
+            className="outline-none text-sm"
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <p className="text-sm text-blue-600 my-4 cursor-pointer">
+          Forgot password
+        </p>
+
+        <button className="bg-blue-600 rounded-full text-white w-full py-2">
+          {state === "Login" ? "Login" : "create account"}
+        </button>
+
+        {state === "Login" ? (
+          <p className="text-center mt-5">
+            Dont have an account?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Sign Up")}
+            >
+              SignUp
+            </span>
+          </p>
+        ) : (
+          <p className="text-center mt-5">
+            Already have an account{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              Login
+            </span>
+          </p>
+        )}
+
+        <img
+          onClick={() => setShowLogin(false)}
+          className="absolute top-5 right-5 cursor-pointer"
+          src={assets.cross_icon}
+          alt=""
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/pages/Result.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+
+const Result = () => {
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
+
+  const onSubmitHandler = async (e) => {};
+
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col min-h-[90vh] justify-center items-center"
+    >
+      <div>
+        <div className="relative ">
+          <img className="max-w-sm rounded " src={image} alt="" />
+          <span
+            className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${
+              loading ? "w-full transition-all duration-[10s]" : "w-0"
+            }`}
+          />
+        </div>
+        <p className={!loading ? "hidden" : ""}>Loading....</p>
+      </div>
+
+      {!isImageLoaded && (
+        <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 my-10 rounded-full  ">
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color "
+            placeholder="Describe what you want to generate"
+            type="text"
+          />
+          <button
+            className="bg-zinc-900 px-10 py-3 sm:px-16 rounded-full"
+            type="submit"
+          >
+            Generate
+          </button>
+        </div>
+      )}
+      {isImageLoaded && (
+        <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full ">
+          <p
+            onClick={() => {
+              setIsImageLoaded(false);
+            }}
+            className="bg-transparent border border-zinc-900 px-8 py-3 rounded-full text-black cursor-pointer"
+          >
+            Generate Another
+          </p>
+          <a
+            href={image}
+            download
+            className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer"
+          >
+            Download
+          </a>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default Result;
